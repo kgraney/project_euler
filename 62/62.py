4de5cf7db9231cc1dd5ca32ac3fa528dcf4eb25e
Kevin Graney <nanonet@gmail.com>
2012-12-15 14:26:49 -0500

    Problem 62 - much faster (and simpler) solution

--- 62/62.py
+++ 62/62.py
@@ -1,46 +1,53 @@
 #!/usr/local/bin/python3
 import itertools
 
-class LookAheadGenerator(object):
-	def __init__(self, gen):
-		self.gen = gen
-		self.buffer = []
+class Record(object):
+	def __init__(self, num):
+		self.count = 0
+		self.num = num
 
-	def __next__(self):
-		if self.buffer:
-			return self.buffer.pop(0)
-		else:
-			return next(self.gen)
+	def __str__(self):
+		return '%d -- %d' % (self.num, self.count)
 
-	def lookahead(self, n=0):
-		while n >= len(self.buffer):
-			try:
-				self.buffer.append(next(self.gen))
-			except StopIteration:
-				return []
-		return self.buffer[n]
+	def __add__(self, rhs):
+		self.count += rhs
+		return self
 
 class CubeIterator(object):
-	def __init__(self):
-		self.seen_cubes = set()
+	def __init__(self, target):
+		self.seen_cubes = {}
+		self.cube_gen = self.cubes_faster()
+		self.target = target 
 
-		tee = itertools.tee(self.cubes_faster(), 2)
-
-		self.cube_gen = tee[0]
-		self.cache_gen = LookAheadGenerator(tee[1])
-
-		self.cache_length = 0
+		self.prev_digits = 0
 
 	def __iter__(self):
 		return self
 
 	def __next__(self):
 		num = next(self.cube_gen)
-		self.advance_seen(len(num))
-		return num
+		digits_num = map(int, str(num))
+		base = tuple(sorted(digits_num))
 
-	def is_cube(self, val):
-		return val in self.seen_cubes
+		if (len(base) > self.prev_digits):
+			self.prev_digits = len(base)
+			result = self.raise_order()
+			if result:	
+				print(result)
+				raise StopIteration
+
+		self.seen_cubes[base] = self.seen_cubes.get(base, Record(num)) + 1
+
+		return base
+
+	def raise_order(self):
+		for k,v in self.seen_cubes.items():
+			if v.count == self.target:
+				return v
+		
+		# clear the map when number of digits changes
+		self.seen_cubes = {}
+		return None
 
 	def cubes_faster(self):
 		odds = itertools.count(1, 2)
@@ -49,34 +56,9 @@ class CubeIterator(object):
 			cube = 0
 			for i in range(0,count):
 				cube += next(odds)
-			yield tuple(map(int, str(cube)))
-
-	def advance_seen(self, length):
-		if (self.cache_length >= length):
-			return
-
-		self.seen_cubes = set()
-		while (len(self.cache_gen.lookahead()) <= length):
-			self.seen_cubes.add(next(self.cache_gen))
-
-		self.cache_length = length
+			yield cube
 
 
-
-target = 1
-cube_iter = CubeIterator()
-for cube in cube_iter:
-	perms = itertools.permutations(cube)
-
-	counted_perms = set()
-	for p in perms:
-		if (p in cube_iter.seen_cubes and not p in counted_perms):
-			counted_perms.add(p)
-
-		if len(counted_perms) > target:
-			break
-
-	if (len(counted_perms) == target):
-		print(target, cube)
-		target += 1
-
+cube_iter = CubeIterator(5)
+for c in cube_iter:
+	pass
