811c9c7a6d57a745a185c8bf6702979f976ca261
Kevin Graney <nanonet@gmail.com>
2012-12-26 17:08:18 -0500

    Problem #267 - changed to parallel IPython job

--- 267/267.py
+++ 267/267.py
@@ -1,17 +1,34 @@
-#!/usr/bin/python3
-import billionaire
+#!/usr/bin/python
+from IPython.parallel import Client
+import numpy 
+
+rc = Client()
+dview = rc[:]
+
+@dview.parallel(block=True)
+def try_fs(lst):
+	import billionaire
+	result = []
+	for f in lst:
+		num_samples = 10
+
+		ratio_sum = 0
+		for i in range(0,num_samples):
+			ratio = billionaire.run_trials(1e4, f)
+			ratio_sum += ratio
+
+		result.append((f, (ratio_sum/num_samples)))
+
+	return result
 
 
-f = 0.1
-while f < 1.0:
+#dview.block = True
+#dview.execute('import billionaire')
+#s = dview.map_sync(lambda x: billionaire.run_trials(1e4, x), f_samples)
 
-	num_samples = 100
+f_samples = numpy.arange(0.208, 0.210, 0.0001)
+print(f_samples)
+samples = try_fs(f_samples)
+for s in samples:
+	print(s)
 
-	ratio_sum = 0
-	for i in range(0,num_samples):
-		ratio = billionaire.run_trials(1e4, f)
-		ratio_sum += ratio
-
-	print(f, '%0.12f' % (ratio_sum/num_samples))
-
-	f += 0.1	
